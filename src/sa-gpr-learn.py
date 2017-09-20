92715926b25d8a69fa60cce84bb70b28d2a0fa18
David M Wilkins <david.wilkins@epfl.ch>
2017-09-20 11:10:15 +0200

    Deleted unused file

--- src/sa-gpr-learn.py
+++
@@ -1,9 +0,0 @@
-#!usr/bin/python
-
-import utils.kernels
-import utils.parsing
-import argparse
-import sys
-import numpy as np
-
-# This is a wrapper that calls python scripts to do SA-GPR with pre-built L-SOAP kernels.
